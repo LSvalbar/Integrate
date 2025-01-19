a48b5a097fa3fe482e57f6f0b1632428ebcdc56a
Pledge <709819499@qq.com>
2025-01-20 00:06:51 +0800

    remove subprocess package

--- IntegrateTool.py
+++ IntegrateTool.py
@@ -1,10 +1,16 @@
 import sys
 import psutil
+import wmi
+import netifaces
+from PyQt5.QtGui import QTextLine
+
 from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QCheckBox, QPushButton, QFrame, QLabel, \
-    QDialog
+    QDialog, QLineEdit, QTextEdit
 import socket
 import uuid
 import subprocess
+
+from psutil import disk_partitions
 
 
 class App(QWidget):
@@ -94,74 +100,105 @@ class App(QWidget):
 class SystemInfoDialog(QDialog):
     def __init__(self, parent=None):
         super().__init__(parent)
-        self.setWindowTitle('System Information')
+        self.setWindowTitle('系统信息')
 
         # 创建布局
-        layout = QVBoxLayout()
-
-        # 创建 Frame 用于显示信息
-        info_frame = QFrame(self)
-        info_layout = QVBoxLayout()
+        layout_label = QVBoxLayout()
+        layout_text = QVBoxLayout()
+        layout = QHBoxLayout()
 
         # 获取并显示系统信息
-        self.display_system_info(info_layout)
+        self.display_system_info(layout_label,layout_text)
 
-        info_frame.setLayout(info_layout)
-        layout.addWidget(info_frame)
+        layout.addLayout(layout_label)
+        layout.addLayout(layout_text)
 
         self.setLayout(layout)
-        self.setFixedSize(400, 300)
+        self.setAutoFillBackground(True)
+        self.setFixedSize(500, 400)
 
-    def display_system_info(self, layout):
+    def display_system_info(self, layout1,layout2):
         # 获取 CPU 型号信息
-        cpu_info = subprocess.check_output(
-            ["powershell", "-Command", "Get-WmiObject -Class Win32_Processor | Select-Object -ExpandProperty Name"],
-            shell=True
-        ).decode().strip()
-        cpu_label = QLabel(f"CPU 型号: {cpu_info}", self)
-        layout.addWidget(cpu_label)
+        systeminfo = wmi.WMI()
+        cpu = systeminfo.Win32_Processor()[0].Name
+        cpu_label = QLabel(self)
+        cpu_label.setText("CPU型号：")
+        layout1.addWidget(cpu_label)
+        cpu_text = QTextEdit(self)
+        cpu_text.setText(cpu)
+        cpu_text.setReadOnly(True)
+        layout2.addWidget(cpu_text)
 
         # 获取硬盘信息
-        disk_count = subprocess.check_output(
-            ["powershell", "-Command", "Get-WmiObject -Class Win32_DiskDrive | Measure-Object"]
-        ).decode().strip().splitlines()[0]
-        disk_capacity = subprocess.check_output(
-            ["powershell", "-Command", "Get-WmiObject -Class Win32_DiskDrive | Select-Object -ExpandProperty Size"],
-            shell=True
-        ).decode().strip().split("\n")
-        disk_capacity = [str(int(capacity) / (1024 ** 3)).split('.')[0] + " GB" for capacity in disk_capacity]
-        disk_label = QLabel(f"硬盘数量: {disk_count.split(':')[1]}，硬盘容量大小: {disk_capacity[0]}", self)
-        layout.addWidget(disk_label)
+        disk_partitions = psutil.disk_partitions()
+        print(disk_partitions)
+        # disk_count = subprocess.check_output(
+        #     ["powershell", "-Command", "Get-WmiObject -Class Win32_DiskDrive | Measure-Object"]
+        # ).decode().strip().splitlines()[0]
+        # disk_capacity = subprocess.check_output(
+        #     ["powershell", "-Command", "Get-WmiObject -Class Win32_DiskDrive | Select-Object -ExpandProperty Size"],
+        #     shell=True
+        # ).decode().strip().split("\n")
+        # disk_capacity = [str(int(capacity) / (1024 ** 3)).split('.')[0] + " GB" for capacity in disk_capacity]
+        # disk_label = QLabel(f"硬盘数量: {disk_count.split(':')[1]}，硬盘容量大小: {disk_capacity[0]}", self)
+        # layout.addWidget(disk_label)
 
         # 获取硬盘序列号
-        disk_serials = subprocess.check_output(
-            ["powershell", "-Command",
-             "Get-WmiObject -Class Win32_DiskDrive | Select-Object -ExpandProperty SerialNumber"],
-            shell=True
-        ).decode().strip().split("\n")
-        disk_serial_label = QLabel(f"硬盘序列号: {disk_serials}", self)
-        layout.addWidget(disk_serial_label)
+        diskinfo = systeminfo.Win32_DiskDrive()[0]
+        disk_serial = diskinfo.SerialNumber.replace(' ', '')
+        disk_number_label = QLabel(self)
+        disk_number_label.setText("硬盘序列号：")
+        layout1.addWidget(disk_number_label)
+        disk_text = QTextEdit(self)
+        disk_text.setText(disk_serial)
+        disk_text.setReadOnly(True)
+        layout2.addWidget(disk_text)
 
         # 获取内存信息
         memory_info = psutil.virtual_memory()
-        memory_label = QLabel(f"内存大小: {memory_info.total / (1024 ** 3):.2f} GB", self)
-        layout.addWidget(memory_label)
+        memory_label = QLabel(self)
+        memory_label.setText("内存：")
+        layout1.addWidget(memory_label)
+        memory_text = QTextEdit(self)
+        memory_text.setText(f"{round(memory_info.total / (1024 ** 3))}GB")
+        memory_text.setReadOnly(True)
+        layout2.addWidget(memory_text)
 
         # 获取 IP 地址
         ip_address = socket.gethostbyname(socket.gethostname())
-        ip_label = QLabel(f"IP 地址: {ip_address}", self)
-        layout.addWidget(ip_label)
+        ip_label = QLabel(self)
+        ip_label.setText("IP地址：")
+        layout1.addWidget(ip_label)
+        ip_text = QTextEdit(self)
+        ip_text.setText(ip_address)
+        ip_text.setReadOnly(True)
+        layout2.addWidget(ip_text)
+
+        # 获取 计算机名
+        comp_name = socket.gethostname()
+        comp_name_label = QLabel(self)
+        comp_name_label.setText("计算机名：")
+        layout1.addWidget(comp_name_label)
+        comp_name_text = QTextEdit(self)
+        comp_name_text.setText(comp_name)
+        comp_name_text.setReadOnly(True)
+        layout2.addWidget(comp_name_text)
 
         # 获取 MAC 地址并格式化
-        mac_addresses = subprocess.check_output(
-            ["powershell", "-Command",
-             "Get-WmiObject -Class Win32_NetworkAdapterConfiguration | Where-Object { $_.IPEnabled -eq $true } | Select-Object -ExpandProperty MACAddress"],
-            shell=True
-        ).decode().strip().split("\n")
-        print()
-        mac_addresses = mac_addresses[0].replace("[\'", "").replace("\']", "")
-        mac_label = QLabel(f"MAC 地址: {mac_addresses}", self)
-        layout.addWidget(mac_label)
+        interfaces = netifaces.interfaces()
+        for interface in interfaces:
+            mac_address = netifaces.ifaddresses(interface)[netifaces.AF_LINK][0]['addr']
+            if mac_address:
+                mac_label = QLabel(self)
+                mac_label.setText("MAC地址：")
+                layout1.addWidget(mac_label)
+                mac_text = QTextEdit(self)
+                mac_text.setText(mac_address)
+                mac_text.setReadOnly(True)
+                layout2.addWidget(mac_text)
+        # mac = uuid.UUID(int = uuid.getnode()).hex[-12:].upper()
+        # mac_addr ="-".join([mac[e:e+2] for e in range(0,len(mac),2)])
+
 
 
 if __name__ == '__main__':
